d5e96277babb853127ce170124097e43724e2a38
Alberto Davila <davila.alb@gmail.com>
2022-05-18 20:30:34 -0500

    Actualizacion de app

---
+++ ObjectDetection/app/src/main/java/org/pytorch/helloworld/ImageNetClasses.java
@@ -0,0 +1,11 @@
+package org.pytorch.helloworld;
+
+public class ImageNetClasses {
+  public static String[] IMAGENET_CLASSES = new String[]{
+      "Cardiomegaly",
+      "Edema",
+      "Consolidation",
+      "Atelectasis",
+      "Pleural Effusion"
+  };
+}
